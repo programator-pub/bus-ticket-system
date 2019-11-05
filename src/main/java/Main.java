54fd45a01876c8bda64803efc9ab4e7bba8f260e
Lukasz <lukasztbg@gmail.com>
2019-11-05 20:09:33 +0100

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
+package main.java;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
